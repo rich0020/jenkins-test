51086dcaad881562165e4fe90e447c6555f1f496
rich0020 <harvey.rich@gmail.com>
2025-11-06 12:41:43 +0000

    Update main.tf

--- main.tf
+++ main.tf
@@ -13,6 +13,6 @@ terraform {
 }
 
 resource "google_compute_network" "example_vpc" {
-  name                    = "example-vpc"
+  name                    = "example-vpc2"
   auto_create_subnetworks = true 
 }
