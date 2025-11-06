5d4d264b98f6f635c9e518a9f3b76a4e88b09c78
rich0020 <harvey.rich@gmail.com>
2025-11-06 11:46:24 +0000

    Update main.tf

--- main.tf
+++ main.tf
@@ -1,65 +1,18 @@
 variable "gcp_project" {}
 
-terraform {
-  required_providers {
-    google = {
-        source = "hashicorp/google"
-        version = "7.7.0"
-    }
-  }
-  backend "gcs" {
-    bucket = "tf-remote-state-student_00_9c8ad21227f6-32720-19984"
-    prefix = "terraform/state/lab00"
-  }
-}
-
 provider "google" {
-    project = var.gcp_project
-    region  = "europe-west1"
+  project = var.gcp_project 
+  region  = "us-west1"
 }
 
-resource "google_compute_instance" "jenkins" {
-  name         = "jenkins"
-  machine_type = "e2-medium"
-  zone         = "europe-west1-b"
-
-  allow_stopping_for_update = true
-  
-  labels = {
-    role = "jenkins_server"
-  }
-  
-  boot_disk {
-    initialize_params {
-      image = "debian-cloud/debian-12"
-    }
-  }
-
-  metadata = {
-    ssh-keys = "ansible:${file("ansible_key.pub")}"
-  }
-  
-  network_interface {
-    network = "default"
-
-    access_config {
-      network_tier = "STANDARD"
-    }
+terraform {
+  backend "gcs" {
+    bucket  = "tf-remote-state-student_00_9c8ad21227f6-32720-19984"
+    prefix  = "terraform/state/lab01" 
   }
 }
 
-resource "google_compute_firewall" "jenkins-firewall" {
-  name    = "jenkins-firewall"
-  network = "default"
-
-  allow {
-    protocol = "tcp"
-    ports    = ["22", "8080"]
-  }
-
-  source_ranges = ["0.0.0.0/0"]
-}
-
-output "jenkins_ip" {
-  value = google_compute_instance.jenkins.network_interface[0].access_config[0].nat_ip
+resource "google_compute_network" "example_vpc" {
+  name                    = "example-vpc"
+  auto_create_subnetworks = true 
 }
